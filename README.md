95384f27249f53d111586e44fe479797cd766287
BitMasher <thebitmasher@gmail.com>
2024-06-09 00:10:27 -0500

    fourth attempt

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | 1       | 156.552 s ±  0.769 s | Mac Studio m2 Ultra | initial implementation, single threaded                                                                           |
 | 2       | 151                  | Mac Studio m2 Ultra | removed all the safety tests                                                                                      |
 | 3       | 115.677 s ±  0.818 s | Mac Studio m2 Ultra | Set reader capacity to 4k, got rid of excess copies, switched to enum pattern matching instead of global city var |
+| 4       | 8.111 s ±  0.311 s   | Mac Studio m2 Ultra | Multi-threaded (cores * 2), ramdisk                                                                               |
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
 #[derive(Clone, Eq, Ord, PartialEq, PartialOrd)]
 struct Measurement {
@@ -13,6 +15,17 @@ struct Measurement {
 }
 
 impl Measurement {
+    fn add_measurement(&mut self, measurement: Measurement) {
+        if measurement.min < self.min {
+            self.min = measurement.min;
+        }
+        if measurement.max > self.max {
+            self.max = measurement.max;
+        }
+        self.sum = self.sum + measurement.sum;
+        self.count = self.count + measurement.count;
+    }
+
     fn take_measure(&mut self, measure: i32) {
         if measure < self.min {
             self.min = measure.clone();
@@ -61,25 +74,85 @@ impl Display for Measurement {
 }
 
 enum State {
+    Init,
     City,
     Temp(String),
+    End
 }
 
 fn main() {
+
+    let (tx, rx) = channel::<HashMap<String, Measurement>>();
+    let filesize = {
+        let measurements_file = File::open("measurements.txt").expect("file not found");
+        measurements_file.metadata().expect("failed to stat file").len()
+    };
+    let thread_count = thread::available_parallelism().expect("failed to fetch cores").get() as u64 * 2;
+    let chunk_size = filesize/thread_count;
+    let excess = filesize% thread_count;
+    for i in 0..thread_count {
+        let tx = tx.clone();
+        let start = chunk_size * i;
+        let size = chunk_size + if i+1 == thread_count { excess } else { 0 };
+        thread::spawn(move|| {
+            process_chunk(start, size as usize, tx);
+        });
+    }
+    let mut measures: HashMap<String, Measurement> = HashMap::new();
+    for _ in 0..thread_count {
+        let segment = rx.recv().unwrap();
+        for i in segment {
+            let m = measures.get_mut(&i.0);
+            match m {
+                None => {
+                    measures.insert(i.0, i.1);
+                }
+                Some(measure) => {
+                    measure.add_measurement(i.1);
+                }
+            }
+        }
+    }
+    let mut vals = measures.into_values().collect::<Vec<Measurement>>();
+    vals.sort_by(|a,b| a.city.cmp(&b.city));
+    for m in vals {
+        println!("{}", m);
+    }
+}
+
+fn process_chunk(start: u64, size: usize, chan: Sender<HashMap<String, Measurement>>) {
     let measurements_file = File::open("measurements.txt").expect("file not found");
     let mut reader = BufReader::with_capacity(4*1024, measurements_file);
+    reader.seek(SeekFrom::Start(if start > 0 { start - 1 } else { start })).expect("Failed to seek to file");
     let mut string_buf: Vec<u8> = vec![];
     let mut measures: HashMap<String, Measurement> = HashMap::new();
     // let mut measures: Vec<Measurement> = vec![];
-    let mut current_state: State = State::City;
+    let mut current_state: State = if start == 0 { State::City } else { State::Init };
+    let mut read_bytes = 0;
+    let mut last_read = false;
+
     loop {
         let mut test_byte: [u8; 1] = [0; 1];
         let test = reader.read(&mut test_byte);
-        if test.is_err() || test.unwrap() == 0 {
+        if test.is_err() {
             break;
+        }
+        let test_size = test.unwrap();
+        if test_size == 0 {
+            break;
+        }
+        read_bytes = read_bytes + test_size;
+        if read_bytes >= size {
+            last_read = true;
         }
 
         match current_state {
+            State::Init => {
+                if test_byte[0] == 10 {
+                    current_state = State::City;
+                }
+                continue;
+            }
             State::City => {
                 if test_byte[0] == 59 {
                     let city_str = String::from_utf8(string_buf.clone()).unwrap();
@@ -106,18 +179,16 @@ fn main() {
                         .unwrap()
                         .parse()
                         .unwrap());
-                    current_state = State::City;
+                    current_state = if last_read { State::End } else { State::City };
                     string_buf.clear();
                     continue;
                 }
 
                 string_buf.push(test_byte[0]);
             }
+            State::End => break
         }
     }
-    let mut vals = measures.into_values().collect::<Vec<Measurement>>();
-    vals.sort_by(|a,b| a.city.cmp(&b.city));
-    for m in vals {
-        println!("{}", m);
-    }
+
+    chan.send(measures).expect("failed to send measurements");
 }
